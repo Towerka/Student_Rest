171c4ee8e85c9213eaa0e4d74ef1fa54bb34f5b1
Towerka <andrey.kozlovskiy.v@gmail.com>
2022-10-27 18:23:06 +0300

    first commit

---
+++ src/main/java/com/example/SpringSql/SpringApp.java
@@ -0,0 +1,21 @@
+package com.example.SpringSql;
+
+import com.example.SpringSql.Student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class SpringApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class, args);
+    }
+}
---
+++ src/main/java/com/example/SpringSql/Student/StudentConfig.java
@@ -0,0 +1,21 @@
+package com.example.SpringSql.Student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(){
+        return args ->{
+            System.out.println("Server started!");
+        };
+    }
+
+}
